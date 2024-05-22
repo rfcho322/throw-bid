2c6eb5e0bda681446e5acc95d436d16470d24469
rfcho322 <98252280+rfcho322@users.noreply.github.com>
2024-05-22 16:29:16 +0800

    added shadcn, tried revalidatePath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { database } from '@/db/database';
+import { bids as bidSchema } from '@/db/schema';
+import { revalidatePath } from 'next/cache';
+
+export default async function Home() {
+
+  const bids = await database.query.bids.findMany();
+
   return (
-    <main className="">
-      Func you
+    <main className="container mx-auto py-12">
+      <form action={async (formData: FormData) => {
+        'use server'
+        // const bid = formData.get("bid") as string;
+        await database.insert(bidSchema).values({});
+        revalidatePath("/");
+      }}>
+        <Input className='text-black' name="bid" type="number" placeholder='Bid' />
+        <Button type='submit'>Place Bid</Button>
+      </form>
+
+
+      {bids.map(bid => (
+        <div key={bid.id}>{bid.id}</div>
+      ))}
     </main>
   );
 }
